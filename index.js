398b34cafcf3a7fa2d6c58387f66a4892eb13133
abbossth <a@test.ru>
2025-12-02 17:15:12 +0500

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Dastur boshlanmoqda...");
+
+// node-telegram-bot-api
+// nodemon --save-dev
+// dotenv
+
+// scriptlar
+//           start
+//           dev
+
+// .gitignore  => node_modules   .env
+
+// BOT_TOKEN to .env
