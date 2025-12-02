c0648ed96b54d98c8b75c1b7800610974788371b
abbossth <a@test.ru>
2025-12-02 17:18:48 +0500

    bot va model papkasi yaratildi

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
+import "./src/bot/bot.js";
+
 console.log("Dastur boshlanmoqda...");
-
-// node-telegram-bot-api
-// nodemon --save-dev
-// dotenv
-
-// scriptlar
-//           start
-//           dev
-
-// .gitignore  => node_modules   .env
-
-// BOT_TOKEN to .env
