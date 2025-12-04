74dcaf24969f7a5f4340d68fc6f029870279da10
abbossth <a@test.ru>
2025-12-04 16:54:41 +0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import mongoose from "mongoose";
 import "./src/bot/bot.js";
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Db is connected...`);
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected!`);
+  });
 
 console.log("Dastur boshlanmoqda...");
