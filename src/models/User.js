09574da67d52eacd5d8edc2fe4bf026e37c885d6
abbossth <a@test.ru>
2025-12-20 16:58:36 +0500

    user model is created

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  chatId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstname: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  action: {
+    type: String,
+    default: "user_created",
+  },
+  name: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+const User = new mongoose.model("User", userSchema);
+
+export default User;
